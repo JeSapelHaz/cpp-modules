c696cc705c0e68aefda28152dc7166026a8ec684
Hasnain Butt <hbutt@mi-c2-p6.s19.be>
2025-02-05 15:09:23 +0100

    premier exo

--- CPP00/megaphone.cpp
+++ CPP00/megaphone.cpp
@@ -6,13 +6,37 @@
 /*   By: hbutt <hbutt@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 18:15:06 by hbutt             #+#    #+#             */
-/*   Updated: 2025/02/04 18:38:46 by hbutt            ###   ########.fr       */
+/*   Updated: 2025/02/05 15:06:47 by hbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string.h>
 
-int main(void)
+void ft_upper(char *str)
 {
-    // printf("Hello World\n");
+    int i = 0;
+    while (str[i])
+    {
+        if (str[i] >= 'a' && str[i] <= 'z')
+            str[i] = str[i] - 32;
+        i++;
+    }
+    std::cout << str;
+}
+int main(int ac, char **av)
+{
+    if (ac == 1)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        return (0);
+    }
+    int i = 1;
+    while (av[i])
+    {
+        ft_upper(av[i]);
+        i++;
+    }
+    std::cout << std::endl;
+    return 0;
 }
