c4c3c356913c177c2df69d1d93311ea064ceeb62
hbutt <hbutt@student.s19.be>
2025-06-16 16:40:49 +0200

    Makefile + main

--- CPP03/ex01/main.cpp
+++ CPP03/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hbutt <hbutt@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 13:46:30 by hbutt             #+#    #+#             */
-/*   Updated: 2025/06/12 17:44:44 by hbutt            ###   ########.fr       */
+/*   Updated: 2025/06/16 16:40:34 by hbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,24 @@
 
 int main() {
     std::cout << "=====================================\n";
+    ClapTrap clap("Willy");
     ScavTrap scav("Serena");
 
     std::cout << "=====================================\n";
     scav.attack("a wild robot");
+    clap.attack("a wild robot");
 
     std::cout << "=====================================\n";
     scav.takeDamage(30);
+    clap.takeDamage(30);
 
     std::cout << "=====================================\n";
     scav.beRepaired(20);
+    clap.beRepaired(20);
 
     std::cout << "=====================================\n";
     scav.guardGate();
+    // clap.guardGate();
 
     std::cout << "=====================================\n";
     return 0;
