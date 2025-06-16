17aa8b0ffcfabccb68ac1fb14fa232abdb54105b
hbutt <hbutt@student.s19.be>
2025-06-16 12:51:01 +0200

    main for ex02

--- CPP02/ex02/main.cpp
+++ CPP02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hbutt <hbutt@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 15:00:12 by hbutt             #+#    #+#             */
-/*   Updated: 2025/06/10 17:40:40 by hbutt            ###   ########.fr       */
+/*   Updated: 2025/06/16 12:50:51 by hbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,41 @@ int main( void ) {
     
     std::cout << b << std::endl;
     
-    std::cout << Fixed::max( a, b ) << std::endl;
+    // std::cout << Fixed::max( a, b ) << std::endl;
+
+    // Fixed a;
+    // Fixed const b(Fixed(5.05f) * Fixed(2));
+
+    // std::cout << "a: " << a << std::endl;
+    // std::cout << "b: " << b << std::endl;
+
+    // std::cout << "++a: " << ++a << std::endl;
+    // std::cout << "a: " << a << std::endl;
+    // std::cout << "a++: " << a++ << std::endl;
+    // std::cout << "a: " << a << std::endl;
+
+    // std::cout << "b: " << b << std::endl;
+
+    // std::cout << "max(a, b): " << Fixed::max(a, b) << std::endl;
+    // std::cout << "min(a, b): " << Fixed::min(a, b) << std::endl;
+
+    // Fixed c(10);
+    // Fixed d(3.5f);
+
+    // std::cout << "c: " << c << std::endl;
+    // std::cout << "d: " << d << std::endl;
+
+    // std::cout << "c + d: " << c + d << std::endl;
+    // std::cout << "c - d: " << c - d << std::endl;
+    // std::cout << "c * d: " << c * d << std::endl;
+    // std::cout << "c / d: " << c / d << std::endl;
+
+    // std::cout << "c > d: " << (c > d) << std::endl;
+    // std::cout << "c < d: " << (c < d) << std::endl;
+    // std::cout << "c >= d: " << (c >= d) << std::endl;
+    // std::cout << "c <= d: " << (c <= d) << std::endl;
+    // std::cout << "c == d: " << (c == d) << std::endl;
+    // std::cout << "c != d: " << (c != d) << std::endl;
 
     return 0;
 }
