a343be089c75d06b5841e53834089c395ec21495
hbutt <hbutt@student.42belgium.be>
2026-01-05 19:06:49 +0100

    all good CPP06

--- CPP06/ex00/ScalarConverter.cpp
+++ CPP06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: hbutt <hbutt@student.42belgium.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/17 14:35:33 by hbutt             #+#    #+#             */
-/*   Updated: 2026/01/05 18:40:39 by hbutt            ###   ########.fr       */
+/*   Updated: 2026/01/05 19:04:43 by hbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,53 +47,59 @@ static void floatConverter(std::string str)
 
 static void doubleConverter(std::string str)
 {
-    if (atoi(str.c_str()) < 0 || atoi(str.c_str()) > 127)
-        std::cout << "char : impossible" << std::endl;
-    else if (atoi(str.c_str()) < 32 || atoi(str.c_str()) == 127)
-        std::cout << "char : non-displayable" << std::endl;
+    double value = atof(str.c_str());
+    
+    if (value < 0 || value > 127)
+        std::cout << "char: impossible" << std::endl;
+    else if (value < 32 || value == 127)
+        std::cout << "char: Non displayable" << std::endl;
     else
-        std::cout << "char : " << static_cast<char>(atof(str.c_str())) << std::endl;
-    std::cout << "int : " <<  static_cast<int>(atof(str.c_str())) << std::endl;
-    std::cout << "float : " << std::fixed << std::setprecision(1) << atof(str.c_str()) << "f" <<std::endl;
-    std::cout << "double : " <<  std::fixed << std::setprecision(1) << static_cast<double>(atof(str.c_str())) << std::endl;
+        std::cout << "char: '" << static_cast<char>(value) << "'" << std::endl;
+    
+    std::cout << "int: " << static_cast<int>(value) << std::endl;
+    std::cout << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(value) << "f" << std::endl;
+    std::cout << "double: " << std::fixed << std::setprecision(1) << value << std::endl;
 }
 
 static void intConverter(std::string str)
 {
-    if (atoi(str.c_str()) < 0 || atoi(str.c_str()) > 127)
-        std::cout << "char : impossible" << std::endl;
-    else if (atoi(str.c_str()) < 32 || atoi(str.c_str()) == 127)
-        std::cout << "char : non-displayable" << std::endl;
+    int value = atoi(str.c_str());
+    
+    if (value < 0 || value > 127)
+        std::cout << "char: impossible" << std::endl;
+    else if (value < 32 || value == 127)
+        std::cout << "char: Non displayable" << std::endl;
     else
-        std::cout << "char : " << static_cast<char>(atof(str.c_str())) << std::endl;
-    std::cout << "int : " <<  atoi(str.c_str()) << std::endl;
-    std::cout << "float : " << std::fixed << std::setprecision(1) << atof(str.c_str()) << "f" <<std::endl;
-    std::cout << "double : " << std::fixed << std::setprecision(1) << static_cast<double>(atof(str.c_str())) << std::endl;
+        std::cout << "char: '" << static_cast<char>(value) << "'" << std::endl;
+    
+    std::cout << "int: " << value << std::endl;
+    std::cout << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(value) << "f" << std::endl;
+    std::cout << "double: " << std::fixed << std::setprecision(1) << static_cast<double>(value) << std::endl;
 }
 
 
 static void minInfConverter()
 {
-    std::cout << "char : impossible" << std::endl;
-    std::cout << "int : impossible" << std::endl;
-    std::cout << "float : -inff" << std::endl;
-    std::cout << "double : -inf" << std::endl;
+    std::cout << "char: impossible" << std::endl;
+    std::cout << "int: impossible" << std::endl;
+    std::cout << "float: -inff" << std::endl;
+    std::cout << "double: -inf" << std::endl;
 }
 
 static void maxInfConverter()
 {
-    std::cout << "char : impossible" << std::endl;
-    std::cout << "int : impossible" << std::endl;
-    std::cout << "float : +inff" << std::endl;
-    std::cout << "double : +inf" << std::endl;
+    std::cout << "char: impossible" << std::endl;
+    std::cout << "int: impossible" << std::endl;
+    std::cout << "float: +inff" << std::endl;
+    std::cout << "double: +inf" << std::endl;
 }
 
 static void nan()
 {
-    std::cout << "char : impossible" << std::endl;
-    std::cout << "int : impossible" << std::endl;
-    std::cout << "float : nanf" << std::endl;
-    std::cout << "double : nan" << std::endl;
+    std::cout << "char: impossible" << std::endl;
+    std::cout << "int: impossible" << std::endl;
+    std::cout << "float: nanf" << std::endl;
+    std::cout << "double: nan" << std::endl;
 }
 
 static void errorMessage() 
