e2ebabfc6db6f9642b6021905bd403ddd46c2458
hbutt <hasnain@live.be>
2026-01-06 17:38:22 +0100

    CPP07 - ex01 finish

--- CPP07/ex01/main.cpp
+++ CPP07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hbutt <hbutt@student.42belgium.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/31 18:07:43 by hbutt             #+#    #+#             */
-/*   Updated: 2025/12/31 18:13:09 by hbutt            ###   ########.fr       */
+/*   Updated: 2026/01/06 17:37:34 by hbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 int main()
 {
     std::cout << "=== Test 1 ===" << std::endl;
-    int array[5] = {1, 2, 3, 4, 5};
-    ::iter(array, 5, ::print_value<int>);
+    const int array[5] = {1, 2, 3, 4, 5};
+    ::iter(array, 5, ::print_value<const int>);
     std::cout << "=== Test 2 ===" << std::endl;
     std::string str[3] = {"comment", "ca va", "les amis"};
     ::iter(str, 3, ::print_value<std::string>);
